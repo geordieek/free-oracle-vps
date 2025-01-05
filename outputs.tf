f922130e349be8c5a46de84ebc60b9770fdf4462
GeordieEK <geordieek@gmail.com>
2025-01-05 20:12:49 +1100

    tf outputs

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,7 @@
-output "public_ip" { value = module.A1-Always-Free-Instance.public_ip }
+output "vps_public_ip" {
+  value = module.A1-Always-Free-Instance.public_ip
+}
+
+output "vps_instance_id" {
+  value = module.A1-Always-Free-Instance.instance_id
+}
